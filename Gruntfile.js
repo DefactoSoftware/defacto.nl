d73668f9135f62db77734248b7c4fac797a2d8c7
romkebon <romke@bonsens.nl>
2013-11-14 14:49:47 +0100

    Combine plugins and scripts to single file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    clean: {
+            dist: 'dist'
+    },
     replace: {
       fancybox: {
         src: ['bower_components/fancybox/source/jquery.fancybox.css'],             // source files array (supports minimatch)
@@ -25,14 +28,15 @@ module.exports = function(grunt) {
     uglify: {
         dist: {
             files: {
-              'dist/uploads/defacto/js/plugins.js': [
+              'dist/uploads/defacto/js/scripts.js': [
+                /*plugins*/
                 'bower_components/fancybox/source/jquery.fancybox.pack.js',
                 'bower_components/jquery.tools/src/scrollable/scrollable.js',
                 'bower_components/jquery.tools/src/scrollable/scrollable.navigator.js',
                 'bower_components/jquery.tools/src/scrollable/scrollable.autoscroll.js',
-                'bower_components/jquery-cycle/jquery.cycle.lite.js'
-              ],
-              'dist/uploads/defacto/js/scripts.js': [
+                'bower_components/jquery-cycle/jquery.cycle.lite.js',
+                
+                /*own scripts*/
                 'scripts/*.js'
               ]
                 
@@ -62,6 +66,7 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "cssmin" task.
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -69,6 +74,7 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', [
+    'clean',
     'replace',
     'cssmin',
     'uglify',
